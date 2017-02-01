e67e7bc5ebaa83ee675155ef10f8b65b2d49dffb
Prasanth podalakur <podalakur.prasanth@kelltontech.com>
2017-02-01 11:40:03 +0530

    Initial Commit

---
+++ SavingData/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SavingData
+//
+//  Created by prasanth.podalakur on 2/1/17.
+//  Copyright © 2017 KelltonTech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
